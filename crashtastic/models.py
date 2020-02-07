8dab5cbd974282e9fc4776e32b397483f98d42bd
chrisgins <christiansgina@gmail.com>
2020-02-07 13:42:11 -0700

    add example views, models, base html

--- crashtastic/models.py
+++ crashtastic/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+
+class Hello(models.Model):
+    when = models.DateTimeField("date created", auto_now_add=True)
--- crashtastic/views.py
+++ crashtastic/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+# Import your .models here...
+from .models import Hello
 
 # Create your views here.
+
+def index(request):
+    # return HttpResponse ('Hello Py-Django!')
+    return render(request, "index.html")
+
+def db(request):
+
+    greeting = Greeting()
+    greeting.save()
+
+    greetings = Greeting.objects.all()
+
+    return render(request, "db.html", {"greetings": greetings})
